874d52bfde3488d849d35620d02e68d16e9dae17
Vlad Kalmykov <vlad.kalmykov@invian.ru>
2024-12-02 18:06:39 +0300

    refine docs a bit

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,26 @@ use std::iter::Sum;
 /// this will produce the sum if all the elements are Ok,
 /// or return the first error
 ///
-/// Note: the implementation of [`Sum`] for T must
+/// # Example
+/// ```
+/// # use advent_of_code_2024::All;
+/// let vals = [1, 2, 3];
+/// // `fallible` is now an iterator of results, so can't be summed
+/// // in a straightforward way
+/// let fallible = vals.into_iter().map(|x| -> Result<u32, ()> {
+///     Ok(x)
+/// });
+/// // All helps with that
+/// let All(res_total) = fallible.map(All).sum();
+/// assert_eq!(res_total, Ok(6));
+///
+/// let fallible = [Ok(1), Err("first"), Ok(3), Err("second")];
+/// let All(res_total) = fallible.into_iter().map(All).sum();
+/// assert_eq!(res_total, Err("first"));
+///
+/// ```
+/// # Note
+/// the implementation of [`Sum`] for T must
 /// respect associotivity in order for this to
 /// work correctly, i.e the following test should pass for
 /// your type
@@ -15,7 +34,6 @@ use std::iter::Sum;
 /// let right: T = [a, b, c].into_iter().sum();
 /// assert_eq!(left, right);
 /// ```
-///
 pub struct All<T, E>(pub Result<T, E>);
 
 impl<T: Sum, E> Sum for All<T, E> {
